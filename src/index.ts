7cce7f175dcb31e3ba550143c93ea368fb1ac755
isinghabhishek <singhabhishek3006.abhi@gmail.com>
2024-04-29 21:02:56 +0530

    add: src/index.js

---
+++ src/index.ts
@@ -0,0 +1,21 @@
+import express from "express";
+import cors from "cors";
+import simpleGit from "simple-git"; 
+import { generate } from "./utils"
+
+const app = express();
+app.use(cors())
+
+//POSTMAN
+app.post("/deploy", async(req, res) => {
+
+})
+
+app.post("/deploy", (req, res) => {
+    const repourl = req.body.repourl;
+    const id = generate(); // asd12
+    await simpleGit().clone(repourl, `output/${id}`);
+
+    res.json({});
+})
+app.listen(3000);
